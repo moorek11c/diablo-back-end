774096af6d867590f9c6e59fb42c9cdbec07737d
Ryan Moore <moorek11c@gmail.com>
2024-12-20 15:18:17 -0800

    fixed gallery

--- controllers/images.js
+++ controllers/images.js
@@ -1,45 +1,4 @@
-const mongoose = require("mongoose");
 const Image = require("../models/imageSchema");
-
-const uploadImage = async (req, res) => {
-  if (!req.file) {
-    console.error("No file uploaded");
-    return res.status(400).send({ message: "No file uploaded" });
-  }
-
-  const newImage = new Image({
-    data: req.file.buffer,
-    contentType: req.file.mimetype,
-  });
-
-  try {
-    const savedImage = await newImage.save();
-
-    res.send({
-      message: "Image uploaded successfully",
-      imageId: savedImage._id,
-    });
-  } catch (err) {
-    console.error("Error saving image:", err);
-    res.status(500).send({ message: "Error saving image" });
-  }
-};
-
-const getImageById = async (req, res) => {
-  try {
-    const imageId = new mongoose.Types.ObjectId(req.params.id);
-    const image = await Image.findById(imageId);
-    if (!image) {
-      return res.status(404).send("Image not found");
-    }
-
-    res.setHeader("Content-Type", image.contentType);
-    res.send(image.data);
-  } catch (err) {
-    console.error("Error fetching image:", err);
-    res.status(500).send({ message: "Error fetching image" });
-  }
-};
 
 const getAllImages = async (req, res) => {
   try {
@@ -51,4 +10,4 @@ const getAllImages = async (req, res) => {
   }
 };
 
-module.exports = { uploadImage, getAllImages };
+module.exports = { getAllImages };
--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,16 +1,7 @@
 const router = require("express").Router;
 const imageController = require("../controllers/images");
 
-// Image upload route
-router.post("/upload", upload.single("image"), imageController.uploadImage);
-
-// Get image by ID route
-router.get("/:id", imageController.getImageById);
-
 // Get all images route
 router.get("/", imageController.getAllImages);
-
-// Delete image by ID route
-router.delete("/:id", imageController.deleteImageById);
 
 module.exports = router;
