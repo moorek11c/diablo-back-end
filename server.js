371749001c93cc8fe0c3b54672c7684e0adf7050
Ryan Moore <moorek11c@gmail.com>
2024-12-19 20:07:10 -0800

    cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const {
 } = require("./utils/errors");
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
     console.error("MongoDB connection error:", err);
@@ -23,8 +23,14 @@ mongoose
   });
 
 app.use(express.json());
-app.use(cors());
 
+app.use(
+  cors({
+    origin: "https://diabloexcavation.com", // Replace with your frontend's domain
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // HTTP methods you allow
+    credentials: true, // Allow cookies or authorization headers
+  })
+);
 app.use("/", indexRoutes);
 
 app.use((req, res, next) => {
